f093b7d6d5e0fef8ae7d0cb81adcfaed92c2c1dd
jack-chiu <jack.cw.chiu@gmail.com>
2021-02-24 14:31:10 +0000

    added dynamic table, still need rest if statements

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ function printWorkout()
   var pez = document.getElementById("pez").value;
   var s = document.getElementById("s").value;
   var sez = document.getElementById("sez").value;
+
+  var pur = "N/A"
+  var pr = "N/A"
+  var sr = "N/A"
+
+  var customWorkout = document.getElementById("customWorkout");
+  customWorkout.rows[1].cells[0].textContent = pu;
+  customWorkout.rows[1].cells[1].textContent = puez;
+  customWorkout.rows[1].cells[2].textContent = pur;
+  customWorkout.rows[2].cells[0].textContent = p;
+  customWorkout.rows[2].cells[1].textContent = pez;
+  customWorkout.rows[2].cells[2].textContent = pr;
+  customWorkout.rows[3].cells[0].textContent = s;
+  customWorkout.rows[3].cells[1].textContent = sez;
+  customWorkout.rows[3].cells[2].textContent = sr;
 }
