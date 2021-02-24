16e7bd516b2cf820ff3a0d4a00e0227e8770c6a1
jack-chiu <jack.cw.chiu@gmail.com>
2021-02-24 17:03:28 +0000

    fix if statements

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 function printWorkout()
 {
   var pu = document.getElementById("pu").value;
-  var puez = document.getElementById("puez").value;
+  var pureps = document.getElementById("pureps").value;
   var p = document.getElementById("p").value;
-  var pez = document.getElementById("pez").value;
+  var preps = document.getElementById("preps").value;
   var s = document.getElementById("s").value;
-  var sez = document.getElementById("sez").value;
+  var sreps = document.getElementById("sreps").value;
 
-  var pur = "N/A"
-  var pr = "N/A"
-  var sr = "N/A"
+  if (pureps = "N/A")
+  {
+    var pusets = "N/A"
+    var purest = "N/A"
+  }
+  else if (pureps = "20")
+  {
+    var pusets = "3"
+    var purest = "30 Seconds"
+  }
 
   var customWorkout = document.getElementById("customWorkout");
   customWorkout.rows[1].cells[0].textContent = pu;
-  customWorkout.rows[1].cells[1].textContent = puez;
-  customWorkout.rows[1].cells[2].textContent = pur;
+  customWorkout.rows[1].cells[1].textContent = pusets;
+  customWorkout.rows[1].cells[2].textContent = pureps;
+  customWorkout.rows[1].cells[3].textContent = purest;
   customWorkout.rows[2].cells[0].textContent = p;
-  customWorkout.rows[2].cells[1].textContent = pez;
-  customWorkout.rows[2].cells[2].textContent = pr;
+  customWorkout.rows[2].cells[1].textContent = psets;
+  customWorkout.rows[2].cells[2].textContent = preps;
+  customWorkout.rows[2].cells[3].textContent = prest;
   customWorkout.rows[3].cells[0].textContent = s;
-  customWorkout.rows[3].cells[1].textContent = sez;
-  customWorkout.rows[3].cells[2].textContent = sr;
+  customWorkout.rows[3].cells[1].textContent = ssets;
+  customWorkout.rows[3].cells[2].textContent = sreps;
+  customWorkout.rows[3].cells[3].textContent = srest;
 }
